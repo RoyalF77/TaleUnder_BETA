aba9b4b1c9ebeb6583f1ccf454123cff8e630311
RoyalF <fabien.villedieu@gmail.com>
2024-04-11 21:52:20 +0200

    Enleve le fichier de Noa qui ne doit pas etre la

--- ALLER ON LE FAIT CE OVERWORLD PTN/view.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-def draw_player(screen, perso, camera):
-    screen.blit(perso.img, camera.apply(perso))
-
-def draw_map(screen, map_image, camera):
-    screen.blit(map_image, camera.apply(map_image))
